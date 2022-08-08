892d4ddd7ba2ff8ae64274956b72f797069e0b9d
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2022-08-08 20:15:17 +0500

    host added

--- FYP/settings.py
+++ FYP/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-2112g+-@$gwa5v=5c(943%r3kow1d!ql+52^-adeam*chn@$+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.100"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",
+                 "192.168.0.100", "https://proscraper.herokuapp.com/"]
 
 
 # Application definition
