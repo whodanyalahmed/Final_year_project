631ddd5d76e84146e644b25c4e639844df8d75a2
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2022-08-08 21:38:03 +0500

    config bin added

--- scrapper_and_analyzer/backend/priceoye.py
+++ scrapper_and_analyzer/backend/priceoye.py
@@ -9,7 +9,7 @@ def Chrome(headless=False):
     chrome_options = webdriver.ChromeOptions()
     if headless:
         chrome_options.add_argument("--headless")
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get(
