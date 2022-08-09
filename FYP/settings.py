8440553ac4442104ed69076754a735ef872912a5
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2022-08-09 20:57:17 +0500

    changes made

--- FYP/settings.py
+++ FYP/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'FYP.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default':{
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proscraper',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
-        
+
     }
 }
 # remove cache
